1e35fd0cbaeb14fc1c2b052ce4f1ee2935abffbd
pablofernandonavarro <pablofernandonavarro@gmail.com>
2024-10-07 17:42:56 -0300

    admin/LTE instalado

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,7 @@
 <?php
 
-
+use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('',function(){
-    return "hola mundo";
-});
+Route::get('',[HomeController::class,'index']);
+
