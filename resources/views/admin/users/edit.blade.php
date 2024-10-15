384ed8b4ed480b3d87c643abcb1cbbb199c4d911
pablofernandonavarro <pablofernandonavarro@gmail.com>
2024-10-15 18:02:44 -0300

    roles in progress 3

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -8,43 +8,43 @@
 
 @section('content')
 @if (session('message'))
-       <div class="alert alert-success">
-        <strong>{{session('message')}}</strong>
-       </div>
+    <div class="alert alert-success">
+        <strong>{{ session('message') }}</strong>
+    </div>
 @endif
-
 
 <div class="card">
     <div class="card-body">
         <p class="h5">Nombre</p>
-        <p class="form-control">{{$user->name}}</p>
+        <p class="form-control">{{ $user->name }}</p>
+
         <h2>Listado de Roles</h2>
-        {!! html()->modelForm($user, 'PUT', route('admin.users.update',$user))->class('form-horizontal p-3')->autocomplete('off')->open() !!}
+
+        {!! html()->modelForm($user, 'PUT', route('admin.users.update', $user))
+            ->class('form-horizontal p-3')
+            ->autocomplete('off')
+            ->open() !!}
         @csrf
         @method('PUT')
+
         <div class="form-group">
-
             @foreach ($roles as $role)
-            <div class="form-check">
-
-                {{ html()->checkbox('roles[]', $user->role, $role->id)
-                ->class('form-check-input')
-                ->id('etiqueta_'.$role->id) }}
-                {{ html()->label($role->name, 'role_'.$role->id)->class('form-check-label') }}
-            </div>
+                <div class="form-check">
+                    {{ html()->checkbox('roles[]', in_array($role->id, $user->roles->pluck('id')->toArray()), $role->id)
+                        ->class('form-check-input')
+                        ->id('role_' . $role->id) }}
+                    {{ html()->label($role->name, 'role_' . $role->id)->class('form-check-label') }}
+                </div>
             @endforeach
+
             <div class="mb-3">
                 {{ html()->submit('Asignar Rol')->class('btn btn-secondary float-right my-2 mx-2') }}
             </div>
 
-            @error('etiquetas')
-            <span class="text-danger">{{ $message }}</span>
+            @error('roles')
+                <span class="text-danger">{{ $message }}</span>
             @enderror
         </div>
-  
-
-
-
 
         {{ html()->form()->close() }}
     </div>
@@ -57,5 +57,4 @@
 @stop
 
 @section('js')
-
 @stop
