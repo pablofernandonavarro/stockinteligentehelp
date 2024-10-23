73046511f8860ffbfd9d271c8c16b01af77c9542
pablofernandonavarro <pablofernandonavarro@gmail.com>
2024-10-23 12:41:08 -0300

    table user responsive 34567

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,22 +2,17 @@
     <div class="max-w-screen-xl mx-auto p-4">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)
-                @php
-                    // Obtener los nombres de las categorías del post
-                    $categories = $post->category->pluck('name');
-                @endphp
-
-                @if (auth()->check())
-                    {{-- Usuario autenticado --}}
-                    @if (auth()->user()->hasRole('Admin'))
-                        {{-- Si es admin, muestra todos los posts --}}
+                {{-- Mostrar los posts excepto los de la categoría "Stock_interna" para usuarios no autenticados o sin el rol Admin --}}
+                @if(auth()->check())
+                    @if(auth()->user()->hasRole('Admin'))
+                        {{-- Si es Admin, muestra todos los posts --}}
                         <article class="w-full h-80 bg-gray-600 @if($loop->first) md:col-span-2 h-96 @endif rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
                             <div class="px-8 py-4 flex flex-col justify-center flex-grow">
                                 <div class="mb-2">
                                     @foreach ($post->etiquetas as $etiqueta)
-                                        <a href="{{ route('posts.etiqueta', $etiqueta) }}" class="inline-block px-3 h-6 text-white rounded-full" style="background-color: {{ $etiqueta->color }};">
-                                            {{ $etiqueta->name }}
-                                        </a>
+                                    <a href="{{ route('posts.etiqueta', $etiqueta) }}" class="inline-block px-3 h-6 text-white rounded-full" style="background-color: {{ $etiqueta->color }};">
+                                        {{ $etiqueta->name }}
+                                    </a>
                                     @endforeach
                                 </div>
 
@@ -29,15 +24,15 @@
                             </div>
                         </article>
                     @else
-                        {{-- Usuario autenticado no admin --}}
-                        @if (!$categories->contains('Stock_interna'))
+                        {{-- Si no es Admin, oculta los posts de la categoría "Stock_interna" --}}
+                        @if($post->category->name != 'Stock_interna')
                             <article class="w-full h-80 bg-gray-600 @if($loop->first) md:col-span-2 h-96 @endif rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
                                 <div class="px-8 py-4 flex flex-col justify-center flex-grow">
                                     <div class="mb-2">
                                         @foreach ($post->etiquetas as $etiqueta)
-                                            <a href="{{ route('posts.etiqueta', $etiqueta) }}" class="inline-block px-3 h-6 text-white rounded-full" style="background-color: {{ $etiqueta->color }};">
-                                                {{ $etiqueta->name }}
-                                            </a>
+                                        <a href="{{ route('posts.etiqueta', $etiqueta) }}" class="inline-block px-3 h-6 text-white rounded-full" style="background-color: {{ $etiqueta->color }};">
+                                            {{ $etiqueta->name }}
+                                        </a>
                                         @endforeach
                                     </div>
 
@@ -51,15 +46,15 @@
                         @endif
                     @endif
                 @else
-                    {{-- Usuario no autenticado --}}
-                    @if (!$categories->contains('Stock_interna'))
+                    {{-- Usuarios no autenticados: Oculta los posts de la categoría "Stock_interna" --}}
+                    @if($post->category->name != 'Stock_interna')
                         <article class="w-full h-80 bg-gray-600 @if($loop->first) md:col-span-2 h-96 @endif rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
                             <div class="px-8 py-4 flex flex-col justify-center flex-grow">
                                 <div class="mb-2">
                                     @foreach ($post->etiquetas as $etiqueta)
-                                        <a href="{{ route('posts.etiqueta', $etiqueta) }}" class="inline-block px-3 h-6 text-white rounded-full" style="background-color: {{ $etiqueta->color }};">
-                                            {{ $etiqueta->name }}
-                                        </a>
+                                    <a href="{{ route('posts.etiqueta', $etiqueta) }}" class="inline-block px-3 h-6 text-white rounded-full" style="background-color: {{ $etiqueta->color }};">
+                                        {{ $etiqueta->name }}
+                                    </a>
                                     @endforeach
                                 </div>
 
