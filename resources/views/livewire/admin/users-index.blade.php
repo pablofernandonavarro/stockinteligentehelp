c97f126ae22582d5b1f417d543fef23dc76ee63e
pablofernandonavarro <pablofernandonavarro@gmail.com>
2024-10-23 11:11:56 -0300

    table user responsive

--- resources/views/livewire/admin/users-index.blade.php
+++ resources/views/livewire/admin/users-index.blade.php
@@ -1,9 +1,9 @@
 <div>
     @if(session('message'))
-<div class="alert alert-success">
-    <strong>{{session('message')}}</strong>
-</div>
-@endif
+    <div class="alert alert-success">
+        <strong>{{session('message')}}</strong>
+    </div>
+    @endif
     <div class="card">
         <div class="card-header">
             <input wire:model.live="search" class="form-control" placeholder="Ingrese el nombre de un Usuario" autofocus />
@@ -12,35 +12,37 @@
 
 
         <div class="card-body">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>Nombre</th>
-                        <th>Email</th>
-                        <th>Accion</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        @foreach ($users as $user )
-                        <td>{{$user->id}}</td>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->email}}</td>
-                        <td with="10px">
-                            <a href="{{route('admin.users.edit',$user)}}" class="btn btn-primary btn-sm">Editar</a>
-                        </td>
-                        <td with="10px">
-                            <form action="{{route('admin.users.destroy', $user)}}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger btn-sm">Eliminar</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>Accion</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            @foreach ($users as $user )
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td with="10px">
+                                <a href="{{route('admin.users.edit',$user)}}" class="btn btn-primary btn-sm">Editar</a>
+                            </td>
+                            <td with="10px">
+                                <form action="{{route('admin.users.destroy', $user)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger btn-sm">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
         <div class="card-footer">
             {{ $users->links() }}
