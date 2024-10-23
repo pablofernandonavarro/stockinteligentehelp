8266a9643d90cab1cc204704430d68048f3888cf
pablofernandonavarro <pablofernandonavarro@gmail.com>
2024-10-23 11:18:16 -0300

    table user responsive 3

--- resources/views/admin/etiquetas/index.blade.php
+++ resources/views/admin/etiquetas/index.blade.php
@@ -17,42 +17,44 @@
         <a href="{{route('admin.etiquetas.create')}}" class="btn btn-success">Agregar Etiqueta</a>
     </div>
     <div class="card-body">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th></th>
-                    <th>Nombre</th>
-                    <th>Color</th>
-                    <th colspan="2"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($etiquetas as $etiqueta)
-                <tr>
-                    <td>{{ $etiqueta->id }}</td>
-                    <td width="5px">
-                        @php
-                        $color = $etiqueta->color ?? '#ffffff'; // Asigna blanco si no hay color
-                        @endphp
-                        <div style="width: 20px; height: 20px; background-color: {{ $etiqueta->color }}; border: 1px solid #000;"></div>
-                    </td>
-                    <td>{{ $etiqueta->name }}</td>
-                    <td>{{ $etiqueta->color }}</td>
-                    <td width="10px">
-                        <a href="{{route('admin.etiquetas.edit',$etiqueta)}}" class="btn btn-sm btn-primary">Editar</a>
-                    </td>
-                    <td width="10px">
-                        <form action="{{route('admin.etiquetas.destroy',$etiqueta)}}" method="POST">
-                            @csrf
-                            @method('delete')
-                            <button class="btn btn-danger btn-sm">Borrar</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th></th>
+                        <th>Nombre</th>
+                        <th>Color</th>
+                        <th colspan="2"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($etiquetas as $etiqueta)
+                    <tr>
+                        <td>{{ $etiqueta->id }}</td>
+                        <td width="5px">
+                            @php
+                            $color = $etiqueta->color ?? '#ffffff'; // Asigna blanco si no hay color
+                            @endphp
+                            <div style="width: 20px; height: 20px; background-color: {{ $etiqueta->color }}; border: 1px solid #000;"></div>
+                        </td>
+                        <td>{{ $etiqueta->name }}</td>
+                        <td>{{ $etiqueta->color }}</td>
+                        <td width="10px">
+                            <a href="{{route('admin.etiquetas.edit',$etiqueta)}}" class="btn btn-sm btn-primary">Editar</a>
+                        </td>
+                        <td width="10px">
+                            <form action="{{route('admin.etiquetas.destroy',$etiqueta)}}" method="POST">
+                                @csrf
+                                @method('delete')
+                                <button class="btn btn-danger btn-sm">Borrar</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 @stop
