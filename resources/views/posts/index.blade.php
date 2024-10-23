232d6bd5226a7dc5c4885f13785a49c0f1ad31a4
pablofernandonavarro <pablofernandonavarro@gmail.com>
2024-10-23 10:40:06 -0300

    issue stock_interna in post2

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,23 +2,26 @@
     <div class="max-w-screen-xl mx-auto p-4">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)
-            <article class="w-full h-80 bg-gray-600 @if($loop->first) md:col-span-2 h-96 @endif rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
-                <div class="px-8 py-4 flex flex-col justify-center flex-grow">
-                    <div class="mb-2">
-                        @foreach ($post->etiquetas as $etiqueta)
-                        <a href="{{ route('posts.etiqueta', $etiqueta) }}" class="inline-block px-3 h-6  text-white rounded-full " style="background-color: {{ $etiqueta->color }}";>
-                            {{ $etiqueta->name }}
-                        </a>
-                        @endforeach
-                    </div>
+                {{-- Verificar si el post no pertenece a la categoría "Stock_interna" --}}
+                @if(!$post->categories->pluck('name')->contains('Stock_interna'))
+                    <article class="w-full h-80 bg-gray-600 @if($loop->first) md:col-span-2 h-96 @endif rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
+                        <div class="px-8 py-4 flex flex-col justify-center flex-grow">
+                            <div class="mb-2">
+                                @foreach ($post->etiquetas as $etiqueta)
+                                <a href="{{ route('posts.etiqueta', $etiqueta) }}" class="inline-block px-3 h-6 text-white rounded-full" style="background-color: {{ $etiqueta->color }};">
+                                    {{ $etiqueta->name }}
+                                </a>
+                                @endforeach
+                            </div>
 
-                    <h1 class="text-2xl md:text-3xl lg:text-4xl text-white leading-8 font-bold">
-                        <a href="{{ route('posts.show', $post) }}">
-                            {{ $post->name }}
-                        </a>
-                    </h1>
-                </div>
-            </article>
+                            <h1 class="text-2xl md:text-3xl lg:text-4xl text-white leading-8 font-bold">
+                                <a href="{{ route('posts.show', $post) }}">
+                                    {{ $post->name }}
+                                </a>
+                            </h1>
+                        </div>
+                    </article>
+                @endif
             @endforeach
         </div>
 
