dd17021f58149544eab191db6e2ce9ec0fb0466a
pablofernandonavarro <pablofernandonavarro@gmail.com>
2024-11-04 16:21:32 -0300

    vista

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
          $this->call(UserSeeder::class);
          Category::factory(4)->create();
          Etiqueta::factory(8)->create();
-        //  $this->call(PostSeeder::class);
+         $this->call(PostSeeder::class);
         $this->call(FAQSeeder::class);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
     <div class="max-w-screen-xl mx-auto p-4">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             @foreach ($posts as $post)
                 {{-- Mostrar los posts excepto los de la categoría "Stock_interna" para usuarios no autenticados o sin el rol Admin --}}
                 @if(auth()->check())
                     @if(auth()->user()->hasRole('Admin'))
                         {{-- Si es Admin, muestra todos los posts --}}
-                        <article class="w-full h-80 bg-gray-600 @if($loop->first) md:col-span-2 h-96 @endif rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
+                        <article class="w-full h-80 bg-gray-600 rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
                             <div class="px-8 py-4 flex flex-col justify-center flex-grow">
                                 <div class="mb-2">
                                     @foreach ($post->etiquetas as $etiqueta)
@@ -26,7 +26,7 @@
                     @else
                         {{-- Si no es Admin, oculta los posts de la categoría "Stock_interna" --}}
                         @if($post->category->name != 'Stock_interna')
-                            <article class="w-full h-80 bg-gray-600 @if($loop->first) md:col-span-2 h-96 @endif rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
+                            <article class="w-full h-80 bg-gray-600 rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
                                 <div class="px-8 py-4 flex flex-col justify-center flex-grow">
                                     <div class="mb-2">
                                         @foreach ($post->etiquetas as $etiqueta)
@@ -48,7 +48,7 @@
                 @else
                     {{-- Usuarios no autenticados: Oculta los posts de la categoría "Stock_interna" --}}
                     @if($post->category->name != 'Stock_interna')
-                        <article class="w-full h-80 bg-gray-600 @if($loop->first) md:col-span-2 h-96 @endif rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
+                        <article class="w-full h-80 bg-gray-600 rounded-lg overflow-hidden shadow-md flex flex-col justify-between">
                             <div class="px-8 py-4 flex flex-col justify-center flex-grow">
                                 <div class="mb-2">
                                     @foreach ($post->etiquetas as $etiqueta)
