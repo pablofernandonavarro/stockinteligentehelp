d4f12c0f567eecdaab87eee04135d3b7608d58e2
pablofernandonavarro <pablofernandonavarro@gmail.com>
2024-11-15 09:14:21 -0300

    branch crud

--- app/Livewire/Branchshow.php
+++ app/Livewire/Branchshow.php
@@ -4,16 +4,87 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Branch;
+
 class Branchshow extends Component
 {
-    public $customerId;
+    public $branches;
+    public $selectedBranch;
+    public $action = 'create';
+    public $showModal = false;
+
+    public function mount()
+    {
+        $this->branches = Branch::all();
+        $this->selectedBranch = new Branch();
+    }
+
+    public function showModal($id = null)
+    {
+        if ($id) {
+            $this->selectedBranch = Branch::find($id);
+
+            // Si no se encuentra la sucursal, inicializar un nuevo objeto.
+            if (!$this->selectedBranch) {
+                $this->selectedBranch = new Branch();
+            }
+
+            $this->action = 'edit'; // Acción de edición
+        } else {
+            $this->selectedBranch = new Branch(); // Inicializa un nuevo objeto Branch
+            $this->action = 'create'; // Acción de creación
+        }
+
+        $this->showModal = true; // Muestra el modal
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'selectedBranch.branch_name' => 'required',
+            'selectedBranch.any_desk' => 'required',
+            'selectedBranch.address' => 'required',
+            'selectedBranch.latitude' => 'required',
+            'selectedBranch.longitude' => 'required',
+        ]);
+
+        $this->selectedBranch->save();
+        $this->showModal = false;
+        $this->branches = Branch::all();
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'selectedBranch.branch_name' => 'required',
+            'selectedBranch.any_desk' => 'required',
+            'selectedBranch.address' => 'required',
+            'selectedBranch.latitude' => 'required',
+            'selectedBranch.longitude' => 'required',
+        ]);
+
+        $this->selectedBranch->save();
+        $this->showModal = false;
+        $this->branches = Branch::all();
+    }
+
+    public function closeModal()
+    {
+        $this->showModal = false;
+        $this->selectedBranch = new Branch();
+    }
 
     public function render()
-    {
-        // Utiliza el modelo Branch para hacer la consulta a la base de datos
-        $branches = Branch::where('customer_id', $this->customerId)->get();
+{
+    if (is_null($this->selectedBranch)) {
+        $this->selectedBranch = new Branch(); // Asegúrate de que sea un objeto válido
+    }
 
-        // Retorna la vista de Livewire con las sucursales
-        return view('livewire.branchshow', compact('branches'));
+    return view('livewire.branchshow');
+}
+    public function showDetails($id)
+    {
+        $this->selectedBranch = Branch::find($id);
+        $this->action = 'show'; // Solo muestra detalles
+        $this->showModal = true; // Muestra el modal
     }
 }
--- resources/views/livewire/branchshow.blade.php
+++ resources/views/livewire/branchshow.blade.php
@@ -2,15 +2,16 @@
     <div class="accordion" id="accordionPanelsStayOpenExample">
         <div class="accordion-item">
             <h2 class="accordion-header">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne"
-                    data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false"
+                    aria-controls="panelsStayOpen-collapseOne">
                     Sucursales
                 </button>
             </h2>
-            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse ">
+            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse">
                 <div class="accordion-body">
                     <div>
-                        <button class='btn btn-success btn-sm mb-3'>Crear Sucursal</button>
+                        <button wire:click="showModal()" class="btn btn-success btn-sm mb-3">Crear Sucursal</button>
                     </div>
 
                     <table class="table table-fixed">
@@ -27,14 +28,20 @@
                                     <td>{{ $branch->branch_name }}</td>
                                     <td>{{ $branch->any_desk }}</td>
                                     <td>
-                                        <a href=""class="btn btn-secondary btn-sm">ver</a>
-                                        <a href=""class="btn btn-secondary btn-sm">edit</a>
-                                        <a href=""class="btn btn-secondary btn-sm">elimi</a>
+                                        <button wire:click="showDetails({{ $branch->id }})" class="btn btn-secondary btn-sm">
+                                            <i class="fas fa-eye"></i> Ver
+                                        </button>
+                                        <button wire:click="showModal({{ $branch->id }})" class="btn btn-secondary btn-sm">
+                                            <i class="fas fa-newspaper"></i> Editar
+                                        </button>
+                                        <button wire:click="delete({{ $branch->id }})" class="btn btn-danger btn-sm">
+                                            <i class="fas fa-trash"></i> Eliminar
+                                        </button>
                                     </td>
                                 </tr>
                             @empty
                                 <tr>
-                                    <td colspan="2">No hay sucursales para este cliente.</td>
+                                    <td colspan="3">No hay sucursales para este cliente.</td>
                                 </tr>
                             @endforelse
                         </tbody>
@@ -43,12 +50,78 @@
             </div>
         </div>
     </div>
+
+    {{-- Modal --}}
+    <div class="modal fade @if ($showModal) show @endif" tabindex="-1"
+        style="@if ($showModal) display: block; @else display: none; @endif" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">
+                        @if ($action === 'create')
+                            Crear Sucursal
+                        @elseif ($action === 'edit')
+                            Editar Sucursal
+                        @else
+                            Detalles de la Sucursal
+                        @endif
+                    </h5>
+                    <button type="button" class="btn-close" wire:click="closeModal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    @if ($action === 'create' || $action === 'edit')
+                        <!-- Formulario de edición o creación -->
+                        <div class="form-group">
+                            <label>Nombre:</label>
+                            <input type="text" class="form-control" wire:model="selectedBranch.branch_name">
+                        </div>
+                        <div class="form-group">
+                            <label>Acceso Remoto:</label>
+                            <input type="text" class="form-control" wire:model="selectedBranch.any_desk">
+                        </div>
+                        <div class="form-group">
+                            <label>Dirección:</label>
+                            <input type="text" class="form-control" wire:model="selectedBranch.address">
+                        </div>
+                        <div class="form-group">
+                            <label>Latitud:</label>
+                            <input type="text" class="form-control" wire:model="selectedBranch.latitude">
+                        </div>
+                        <div class="form-group">
+                            <label>Longitud:</label>
+                            <input type="text" class="form-control" wire:model="selectedBranch.longitude">
+                        </div>
+                    @elseif ($action === 'show')
+                        <!-- Solo muestra detalles -->
+                        <p><strong>Nombre:</strong> {{ $selectedBranch?->branch_name }}</p>
+                        <p><strong>Acceso Remoto:</strong> {{ $selectedBranch->any_desk }}</p>
+                        <p><strong>Dirección:</strong> {{ $selectedBranch->address }}</p>
+                        <p><strong>Latitud:</strong> {{ $selectedBranch->latitude }}</p>
+                        <p><strong>Longitud:</strong> {{ $selectedBranch->longitude }}</p>
+                        <p><strong>Creado:</strong> {{ \Carbon\Carbon::parse($selectedBranch->created_at)->format('d-m-Y') }}</p>
+                        <p><strong>Actualizado:</strong> {{ \Carbon\Carbon::parse($selectedBranch->updated_at)->format('d-m-Y') }}</p>
+                    @endif
+                </div>
+                <div class="modal-footer">
+                    @if ($action === 'create')
+                        <button type="button" class="btn btn-primary" wire:click="save">Guardar</button>
+                    @elseif ($action === 'edit')
+                        <button type="button" class="btn btn-primary" wire:click="update">Actualizar</button>
+                    @endif
+                    <button type="button" class="btn btn-secondary" wire:click="closeModal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
 @section('css')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 @stop
 
 @section('js')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
 @stop
