b8c5ca0ec781fff888313c8162d201bda541b72b
pablofernandonavarro <pablofernandonavarro@gmail.com>
2024-12-04 17:17:16 -0300

    formulario envianos tu consultasolo vistas

--- resources/views/faqs/formfaqs.blade.php
+++ resources/views/faqs/formfaqs.blade.php
@@ -8,9 +8,9 @@
             <div class="flex flex-wrap lg:justify-between -mx-4">
                 <div class="w-full lg:w-1/2 xl:w-6/12 px-4">
                     <div class="max-w-[570px] mb-12 lg:mb-0">
-                        <span class="block mb-4 text-base text-primary font-semibold">
+                        {{-- <span class="block mb-4 text-base text-primary font-semibold">
                             Contactanos :
-                        </span>
+                        </span> --}}
                         <h2
                             class="
                   text-dark
@@ -22,15 +22,13 @@
                   lg:text-[36px]
                   xl:text-[40px]
                   ">
-                            Ingresa tu consulta :
+                            Contactanos :
                         </h2>
                         <p class="text-base text-body-color leading-relaxed mb-9">
-                            Ingresa tu consulta dentro el campo consulta a la brevedad un equipo
-                            de Stock Inteligente te respondera!
+                            Completa el siguiente formulario y dentro de las próximas 24 hrs hábiles te contactará uno
+                            de nuestros asesores.
                         </p>
-                        <span>
-                            Gracias por ayudarnos a crecer !!
-                        </span>
+
 
                     </div>
                 </div>
@@ -63,6 +61,16 @@
                         focus:border-primary
                         " />
                                 @error('email')
+                                    <div class= "text-red-500">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="mb-6">
+                                <input type="text" placeholder="Empresa" name="company"
+                                    class="w-full rounded py-3 px-[14px] text-body-color text-base border border-[f0f0f0] outline-none focus-visible:shadow-none focus:border-primary" />
+
+                                @error('name')
                                     <div class= "text-red-500">
                                         {{ $message }}
                                     </div>
