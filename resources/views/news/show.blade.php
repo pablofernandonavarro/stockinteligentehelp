417f387e18d6dd3bf65bed340483ee73ccb61246
pablofernandonavarro <pablofernandonavarro@gmail.com>
2025-01-22 16:10:05 -0300

    arreglando vistas

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-   
+
     <div class="flex justify-center items-center min-h-screen bg-gray-100 ">
         <div class="max-w-3xl w-full bg-white shadow-md rounded-lg overflow-hidden border border-gray-300">
             <!-- Imagen de la noticia -->
@@ -20,18 +20,17 @@
 
                 <!-- Subtítulo -->
                 <h2 class="text-xl font-semibold text-gray-600 mb-4 text-center">
-                   
+
                 </h2>
 
                 <!-- Contenido -->
                 <p class="text-gray-700 text-lg leading-relaxed mb-4 text-justify">
-               {!!$news->content!!}
+                    {!!$news->content!!}
                 </p>
 
                 <!-- Botón de acción -->
                 <div class="mt-6 text-center">
-                    <a href="{{route('news.index')}}"
-                        class="inline-block px-6 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 transition">
+                    <a href="{{ route('news.index') }}" class="px-4 py-2 bg-blue-500 text-white rounded">
                         Leer más Novedades
                     </a>
                 </div>
